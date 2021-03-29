d1a261fe15e6841315090d1e955dba74896ba06c
Kv <kv.android@gmail.com>
2021-03-29 20:05:45 +0700

    Add change language

--- app/src/main/java/com/kvsoftware/changelanguage/MainActivity.kt
+++ app/src/main/java/com/kvsoftware/changelanguage/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.kvsoftware.changelanguage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button_english).setOnClickListener {
+            changeLanguage("en_US")
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        findViewById<Button>(R.id.button_thai).setOnClickListener {
+            changeLanguage("th")
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun changeLanguage(language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config: Configuration = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 }
