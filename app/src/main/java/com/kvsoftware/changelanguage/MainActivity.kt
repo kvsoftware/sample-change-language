caa0d05913bc1f9daf5b3b5ae997fb8aca9384c7
Kv <kv.android@gmail.com>
2021-03-30 23:20:16 +0700

    Add sharepref for saving language

--- app/src/main/java/com/kvsoftware/changelanguage/MainActivity.kt
+++ app/src/main/java/com/kvsoftware/changelanguage/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.kvsoftware.changelanguage
 
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
@@ -8,27 +10,35 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val LANGUAGE_EN = "en_US"
+        const val LANGUAGE_TH = "th"
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        val language = SharedPreferences(newBase).language ?: LANGUAGE_EN
+        val localeUpdatedContext = CustomWrapper.updateLocale(newBase, Locale(language))
+        super.attachBaseContext(localeUpdatedContext)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button_english).setOnClickListener {
-            changeLanguage("en_US")
-            finish()
-            startActivity(Intent(this, MainActivity::class.java))
+            SharedPreferences(this).language = LANGUAGE_EN
+            openMainActivity()
         }
         findViewById<Button>(R.id.button_thai).setOnClickListener {
-            changeLanguage("th")
-            finish()
-            startActivity(Intent(this, MainActivity::class.java))
+            SharedPreferences(this).language = LANGUAGE_TH
+            openMainActivity()
         }
     }
 
-    private fun changeLanguage(language: String) {
-        val locale = Locale(language)
-        Locale.setDefault(locale)
-        val config: Configuration = resources.configuration
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
+    private fun openMainActivity() {
+        finish()
+        startActivity(Intent(this, MainActivity::class.java))
     }
+
 }
